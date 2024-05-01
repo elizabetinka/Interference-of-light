5751fad2e577a7c7079de255238afca9eec2c436
Елизавета Кравченкова <elizaveta.kravchenkova@mail.ru>
2024-05-02 00:42:35 +0300

    fix l=1

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ ctx.fillRect(0, 0, width, height);
 
 let waveLenght = 600*Math.pow(10,-9);
 let koef = 1.5;
-let d = 0.0005;
-let L = 1.7;
+let d = 0.0000005;
+let L = 1;
 
 
 const availableScreenWidth = window.screen.availWidth;
@@ -28,7 +28,7 @@ showMessage(waveLenght,koef,d,L);
 
 
 function  showMessage( waveLenght ,koef,d,L){
-    let I0=1/2;    
+    let I0=1;    
     
     let width2=waveLenght*L/d;
     console.log("Ширина", width2);
@@ -39,14 +39,19 @@ function  showMessage( waveLenght ,koef,d,L){
   console.log(clickX);
   console.log(clickY);
 
-    for (let x =height/2; x>-height/2; x -=1.5){
-        let I = 4 * I0 * Math.cos(Math.PI * koef * d * x / (waveLenght * L))* I0 * Math.cos(Math.PI * koef * d * x / (waveLenght * L));
-        ctx.fillStyle = `rgb(${I*255}, ${I*255}, ${I*255})`;
-        //console.log("I: ",I );
-        ctx.fillRect(0,height/2-x, width, height/2-x+1.5);
-    }
 
+  for (let x =height/2; x>-height/2; x -=1.5555){
+    let I = 4 * I0 * Math.cos(Math.PI * koef * d * x / (waveLenght * L)) * Math.cos(Math.PI * koef * d * x / (waveLenght * L));
+    console.log("I: ",I );
+    let color=(I/4*255)%256;
+    ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
+    console.log("color: ",color );
+    ctx.fillRect(0,height/2-x, width, height/2-x+1.5555);
 }
+
+  
+}
+
 
 
 
